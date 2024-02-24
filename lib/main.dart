149ae0849b8dd000078ff06ace8e85069d17256a
uzbmb <githubsobir@gmail.com>
2024-02-24 11:19:42 +0500

    all

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
 import 'dart:io';
 
 import 'package:connection_notifier/connection_notifier.dart';
-import 'package:conx/firts_part/login_reg/choose_page/main_auth_page.dart';
 import 'package:conx/firts_part/login_reg/enter_page/lang_model/choose_lang.dart';
-import 'package:conx/firts_part/login_reg/sms/sms_page.dart';
-import 'package:conx/firts_part/login_reg/splash/splash_page.dart';
 import 'package:conx/widgets/saved_box.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
-import 'firts_part/login_reg/enter_page/user_category/user_category.dart';
-import 'firts_part/login_reg/reg/add_photo/add_photo.dart';
-import 'firts_part/login_reg/reg/add_row1/add_row.dart';
-import 'firts_part/login_reg/reg/reg.dart';
-import 'firts_part/user_fill/user_birth.dart';
-import 'firts_part/user_fill/user_fill.dart';
 
 class MyHttpOverrides extends HttpOverrides {
   @override
@@ -62,6 +52,7 @@ void main() async {
   );
 }
 
+@immutable
 class MyApp extends ConsumerWidget {
   MyApp({super.key});
 
@@ -74,6 +65,6 @@ class MyApp extends ConsumerWidget {
         supportedLocales: context.supportedLocales,
         locale: context.locale,
         debugShowCheckedModeBanner: false,
-        home:  ChooseLang());
+        home: ChooseLang());
   }
 }
